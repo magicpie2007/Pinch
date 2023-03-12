f1a7ee99cdb1158097d87089604e638199275180
magcipie2007 <magicpie2007@gmail.com>
2023-03-12 17:44:21 +0900

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by hiroyuki shoji on 2023/03/12.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
